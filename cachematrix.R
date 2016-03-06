863fed2e37e8505d4498b3b7588a10197bf417ce
Boris Kopin <kopbob@gmail.com>
2016-03-07 00:12:05 +0300

    Assignment2 implementation.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment: Programming Assignment 2: Lexical Scoping
 
-## Write a short comment describing this function
+## This function creates a special "matrix"
+## object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  list(set = set,
+       get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix"
+## returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed),
+## then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) inv
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
+
+## Small test
+# m <- makeCacheMatrix()
+# m$set(matrix(c(1:4), nrow = 2))
+# m_inv <- cacheSolve(m)
+# stopifnot(all(m_inv == solve(m$get())))
+# stopifnot(all(m$getinv() == solve(m$get())))
